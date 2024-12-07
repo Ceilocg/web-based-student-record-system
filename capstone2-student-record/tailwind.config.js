2a4f3c6bc3e4e061c0616834413ed1326b4b4df8
Ceilocg <ceilofurogabotero@gmai.com>
2024-12-07 22:24:23 +0800

    update

--- capstone2-student-record/tailwind.config.js
+++ capstone2-student-record/tailwind.config.js
@@ -1,22 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // Ensure you're scanning the correct files for Tailwind classes
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        pink: {
-          100: '#ffccd5', // Light pink
-          300: '#ff99a8', // Medium pink
-          500: '#ff667f', // Darker pink
-          700: '#ff3361', // Dark pink
-        },
-      },
-    },
+    extend: {},
   },
-  darkMode: 'class', // Enable class-based dark mode
   plugins: [],
 }
-// tailwind.config.js
